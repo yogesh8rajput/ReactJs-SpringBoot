fb856586a0102e265c2efb5b388e2d4c017a9905
Deepti Mahisare <deeptimahisare@gmail.com>
2025-02-01 12:01:19 +0530

    check

--- Backend_Product_Manage/src/main/java/com/backend/config/SecurityConfig.java
+++ Backend_Product_Manage/src/main/java/com/backend/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -70,5 +71,11 @@ public class SecurityConfig {
 		UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**",configuration);
 		return (CorsConfigurationSource) source;
+	}
+	
+	
+	@Bean
+	public UserDetailsService userDetailsService2{
+		return new InMemoryUserDetailsManager()
 	}
 }
