fd000362f1df82d1734407cd27bdf2e0a260c577
yogesh8rajput <yogeshmyrajput38203@gmail.com>
2025-03-28 22:40:16 +0530

    27 march

--- FrontEnd_ReactJs/src/axiosConfig.jsx
+++ FrontEnd_ReactJs/src/axiosConfig.jsx
@@ -8,6 +8,7 @@ import axios from "axios";
   
   axiosInstance.interceptors.request.use(
     (config) => {
+      
         const configs = {
             API_URL: 'http://localhost:8090/',
             API_USERNAME: 'hello',
