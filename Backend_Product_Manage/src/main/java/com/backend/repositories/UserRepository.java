795397276e7234ac114e76513ac4e99d2900b44c
Deepti Mahisare <deeptimahisare@gmail.com>
2025-07-09 22:13:23 +0530

    repository

--- Backend_Product_Manage/src/main/java/com/backend/repositories/UserRepository.java
+++ Backend_Product_Manage/src/main/java/com/backend/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+  //to find the user by its username
 	User findByUsername(String username);
 
 }
