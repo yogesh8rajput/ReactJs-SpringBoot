1d5e90ad9ff6a3585d8f9d33c3121b5075c1278c
Deepti Mahisare <deeptimahisare@gmail.com>
2025-07-14 22:58:04 +0530

    context

--- FrontEnd_ReactJs/src/Components/ProtectedRoute.jsx
+++ FrontEnd_ReactJs/src/Components/ProtectedRoute.jsx
@@ -16,18 +16,3 @@ const ProtectedRoute=({children})=>{
 export default ProtectedRoute;
 
 
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "./AuthContext";
-
-// const ProtectedRoute = ({ children }) => {
-//   const { isAuthenticated } = useAuth();
-
-//   if (!isAuthenticated) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
