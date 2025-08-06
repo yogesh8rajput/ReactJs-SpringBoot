74f535c2c41bced58f5108c36b3fe17640ab5cf5
Deepti Mahisare <deeptimahisare@gmail.com>
2025-08-06 23:07:22 +0530

    role based auth

--- Backend_Product_Manage/src/main/java/com/backend/entity/Users.java
+++ Backend_Product_Manage/src/main/java/com/backend/entity/Users.java
@@ -1,5 +1,27 @@
 package com.backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="RoleUsers")
 public class Users {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String name;
+	
+	@Column(unique = true)
+	private String email;
+	
+	private String password;
+	
+	private Role role;
 
 }
