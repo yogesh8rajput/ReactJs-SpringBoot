4131bdcad1bd947f88a917adc7220c1d04098646
yogesh8rajput <159117998+yogesh8rajput@users.noreply.github.com>
2025-11-16 21:36:46 +0530

    Update BackendProductManageApplicationTests.java

--- Backend_Product_Manage/src/test/java/com/backend/BackendProductManageApplicationTests.java
+++ Backend_Product_Manage/src/test/java/com/backend/BackendProductManageApplicationTests.java
@@ -5,7 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendProductManageApplicationTests {
-
+//Try to make the test cases..
+	
 	@Test
 	void contextLoads() {
 	}
